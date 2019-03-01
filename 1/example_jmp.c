3b89ab668ad8ab1b8e550426d4d0c95d26d750a3
Vladislav Shpilevoy <v.shpilevoy@tarantool.org>
2019-03-01 20:11:04 +0300

    Add more details on the first homework
    Put a lot of new comments into the examples code; forbid
    quadratic sorts; put more 'calming' and clarifying text into the
    task description.

--- 1/example_jmp.c
+++ 1/example_jmp.c
@@ -2,34 +2,83 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+/**
+ * You can compile and run this example using the commands:
+ *
+ * $> gcc example_jmp.c
+ * $> ./a.out
+ */
+
+/**
+ * This struct describes one single coroutine. It stores its
+ * local variables and a position, where it stands now.
+ */
 struct task {
-        int id;
+        /**
+         * Important thing - execution position where that
+         * coroutine stopped last time.
+         */
         jmp_buf env;
+        /**
+         * This flag is set when the coroutine has finished its
+         * task. It is used to wait until all the coroutines are
+         * finished.
+         */
         bool is_finished;
+	/**
+	 * Just local variables for arbitrary usage. You can put
+	 * here whatever you want.
+	 */
+        int id;
 	char local_data[128];
 };
 
+/**
+ * In your code it should be dynamic, not const. Here it is 3 for
+ * simplicity.
+ */
 #define task_count 3
 static struct task tasks[task_count];
+/**
+ * Index of the currently working coroutine. It is used to learn
+ * which coroutine should be scheduled next, and to get your
+ * current local variables.
+ */
 static int current_task_i = 0;
 
 /**
- * Check not in a function, because setjmp result can not be used
- * after return.
+ * This macro stops the current coroutine and switches to another
+ * one. Check is not in a function, because setjmp result can not
+ * be used after return. You should keep it as macros.
  *
- * Another way how to do not put 'check_resched' after each line -
- * do "define ; check_resched;".
+ * Below you can see, that 'check_resched' is done after each
+ * line. But another way how to do not put 'check_resched' after
+ * each line - do "define ; check_resched;". Then each ';' will
+ * turn into a context switch.
  */
 #define check_resched {						\
-	/* For an example: just resched after each line. */	\
+	/*							\
+	 * For an example: just resched after each line, without\
+	 * timeslices.						\
+	 */							\
 	int old_i = current_task_i;				\
 	current_task_i = (current_task_i + 1) % task_count;	\
 	if (setjmp(tasks[old_i].env) == 0)			\
 		longjmp(tasks[current_task_i].env, 1); }
 
+/**
+ * Coroutine body. This code is executed by all the corutines at
+ * the same time, but with different struct task. Here you
+ * implement your solution.
+ */
 static void
 my_coroutine()
 {
+	/*
+	 * Note - all the data access is done via 'current_task'.
+	 * It is not safe to store anything in local variables
+	 * here.
+	 */
         sprintf(tasks[current_task_i].local_data, "Local data for task_id%d",
         	current_task_i);
 	fprintf(stderr, "Before re-schedule: task_id = %d\n", current_task_i);
@@ -42,6 +91,10 @@ my_coroutine()
 	tasks[current_task_i].is_finished = true;
 	fprintf(stderr, "This is local data: %s\n",
 		tasks[current_task_i].local_data);
+	/*
+	 * All the things below are just waiting for finish of all
+	 * of the coroutines.
+	 */
 	while (true) {
 		bool is_all_finished = true;
 		for (int i = 0; i < task_count; ++i) {
@@ -63,8 +116,7 @@ my_coroutine()
 int
 main(int argc, char **argv)
 {
-        int i;
-        for (i = 0; i < task_count; ++i) {
+        for (int i = 0; i < task_count; ++i) {
                 tasks[i].id = i;
                 tasks[i].is_finished = false;
 		/* Entry point for new co-coutines. */
--- 1/example_swap.c
+++ 1/example_swap.c
@@ -5,6 +5,19 @@
 #include <signal.h>
 #include <sys/mman.h>
 
+/**
+ * You can compile and run this example using the commands:
+ *
+ * $> gcc example_swap.c
+ * $> ./a.out
+ */
+
+/**
+ * Here you have 3 contexts - one for each coroutine. First
+ * belongs to main coroutine - the one, who starts all others.
+ * Next two are worker coroutines - they do real work. In your
+ * code you will have more than 2.
+ */
 static ucontext_t uctx_main, uctx_func1, uctx_func2;
 
 #define handle_error(msg) \
@@ -12,21 +25,32 @@ static ucontext_t uctx_main, uctx_func1, uctx_func2;
 
 #define stack_size 1024 * 1024
 
+/**
+ * Coroutine body. This code is executed by all the corutines at
+ * the same time and with different stacks. So you can store any
+ * local variables and arguments right here. Here you implement
+ * your solution.
+ */
 static void
-func1(int id)
+my_coroutine(int id)
 {
 	printf("func%d: started\n", id);
 	if (id == 1) {
-	        printf("func1: swapcontext(&uctx_func1, &uctx_func2)\n");
+	        printf("coroutine1: swapcontext(&uctx_func1, &uctx_func2)\n");
 		if (swapcontext(&uctx_func1, &uctx_func2) == -1)
 	        	handle_error("swapcontext");
 	} else {
-		printf("func2: swapcontext(&uctx_func2, &uctx_func1)\n");
+		printf("coroutine2: swapcontext(&uctx_func2, &uctx_func1)\n");
 		if (swapcontext(&uctx_func2, &uctx_func1) == -1)
 			handle_error("swapcontext");
 	}
 	printf("func%d: returning\n", id);
 }
+
+/**
+ * Below you can see 3 different ways of how to allocate stack.
+ * You can choose any. All of them do in fact the same.
+ */
 
 static void *
 allocate_stack_sig()
@@ -61,6 +85,10 @@ enum stack_type {
 	STACK_MPROT
 };
 
+/**
+ * Use this wrapper to choose your favourite way of stack
+ * allocation.
+ */
 static void *
 allocate_stack(enum stack_type t)
 {
@@ -77,15 +105,30 @@ allocate_stack(enum stack_type t)
 int
 main(int argc, char *argv[])
 {
+	/* First of all, create a stack for each coroutine. */
 	char *func1_stack = allocate_stack(STACK_SIG);
 	char *func2_stack = allocate_stack(STACK_MPROT);
 
+	/*
+	 * Below is just initialization of coroutine structures.
+	 * They are not started yet. Just created.
+	 */
 	if (getcontext(&uctx_func1) == -1)
 		handle_error("getcontext");
+	/*
+	 * Here you specify a stack, allocated earlier. Unique for
+	 * each coroutine.
+	 */
 	uctx_func1.uc_stack.ss_sp = func1_stack;
 	uctx_func1.uc_stack.ss_size = stack_size;
+	/*
+	 * Important - here you specify, to which context to
+	 * switch after this coroutine is finished. The code below
+	 * says, that when 'uctx_func1' is finished, it should
+	 * switch to 'uctx_main'.
+	 */
 	uctx_func1.uc_link = &uctx_main;
-	makecontext(&uctx_func1, func1, 1, 1);
+	makecontext(&uctx_func1, my_coroutine, 1, 1);
 
 	if (getcontext(&uctx_func2) == -1)
 		handle_error("getcontext");
@@ -93,8 +136,12 @@ main(int argc, char *argv[])
 	uctx_func2.uc_stack.ss_size = stack_size;
 	/* Successor context is f1(), unless argc > 1. */
 	uctx_func2.uc_link = (argc > 1) ? NULL : &uctx_func1;
-	makecontext(&uctx_func2, func1, 1, 2);
+	makecontext(&uctx_func2, my_coroutine, 1, 2);
 
+	/*
+	 * And here it starts. The first coroutine to start is
+	 * 'uctx_func2'.
+	 */
 	printf("main: swapcontext(&uctx_main, &uctx_func2)\n");
 	if (swapcontext(&uctx_main, &uctx_func2) == -1)
 		handle_error("swapcontext");
