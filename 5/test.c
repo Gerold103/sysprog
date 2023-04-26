3c27bc835b554f92041857ba3b87b5ca0d0b7dc4
Vladislav Shpilevoy <v.shpilevoy@tarantool.org>
2023-04-27 00:48:56 +0200

    hw5 chat: add a test for big messages
    This should cover partial writes and reads, and necessity to wait
    for out-event in epoll/poll/kqueue.

--- 5/test.c
+++ 5/test.c
@@ -118,12 +118,73 @@ test_basic(void)
 	unit_test_finish();
 }
 
+static void
+test_big_messages(void)
+{
+	unit_test_start();
+
+	struct chat_server *s = chat_server_new();
+	unit_fail_if(chat_server_listen(s, 0) != 0);
+	uint16_t port = server_get_port(s);
+	struct chat_client *c1 = chat_client_new("c1");
+	unit_fail_if(chat_client_connect(c1, make_addr_str(port)) != 0);
+
+	uint32_t len = 1024 * 1024;
+	uint32_t size = len + 1;
+	char *data = malloc(size);
+	for (uint32_t i = 0; i < len; ++i)
+		data[i] = 'a' + i % ('z' - 'a' + 1);
+	data[len] = '\n';
+	int rc;
+	struct chat_message *msg;
+	int count = 100;
+	int real_count = 0;
+	for (int i = 0; i < count; ++i) {
+		unit_fail_if(chat_client_feed(c1, data, size) != 0);
+		rc = chat_client_update(c1, 0);
+		unit_fail_if(rc != 0 && rc != CHAT_ERR_TIMEOUT);
+		rc = chat_server_update(s, 0);
+		unit_fail_if(rc != 0 && rc != CHAT_ERR_TIMEOUT);
+		while ((msg = chat_server_pop_next(s)) != NULL) {
+			++real_count;
+			size_t msg_len = strlen(msg->data);
+			unit_fail_if(len != msg_len);
+			unit_fail_if(memcmp(msg->data, data, len) != 0);
+			chat_message_delete(msg);
+		}
+	}
+	while (true) {
+		int rc1 = chat_client_update(c1, 0);
+		unit_fail_if(rc1 != 0 && rc1 != CHAT_ERR_TIMEOUT);
+		int rc2 = chat_server_update(s, 0);
+		unit_fail_if(rc2 != 0 && rc2 != CHAT_ERR_TIMEOUT);
+		while ((msg = chat_server_pop_next(s)) != NULL) {
+			++real_count;
+			size_t msg_len = strlen(msg->data);
+			unit_fail_if(len != msg_len);
+			unit_fail_if(memcmp(msg->data, data, len) != 0);
+			chat_message_delete(msg);
+		}
+		if (rc1 == CHAT_ERR_TIMEOUT && rc2 == CHAT_ERR_TIMEOUT)
+			break;
+	}
+	free(data);
+	unit_check(real_count == count, "server got all msgs");
+	unit_check(chat_server_pop_next(s) == NULL, "server has no msgs");
+	unit_check(chat_client_pop_next(c1) == NULL, "client has no msgs");
+	chat_client_delete(c1);
+	chat_server_delete(s);
+
+	unit_test_finish();
+}
+
 int
 main(void)
 {
 	unit_test_start();
 
 	test_basic();
+	test_big_messages();
 
 	unit_test_finish();
 	return 0;
