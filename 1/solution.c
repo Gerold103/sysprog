27324cb633f13ee9f54366e6d545e1cd82de1908
IVIosab <mosab.f.r@gmail.com>
2023-09-21 15:30:08 +0300

    Add file parsing functionality

--- 1/solution.c
+++ 1/solution.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include "libcoro.h"
+#include <malloc.h>
 
 /**
  * You can compile and run this code using the commands:
@@ -10,7 +11,8 @@
  * $> ./a.out
  */
 
-struct my_context {
+struct my_context
+{
 	char *name;
 	/** ADD HERE YOUR OWN MEMBERS, SUCH AS FILE NAME, WORK TIME, ... */
 };
@@ -68,23 +70,78 @@ coroutine_func_f(void *context)
 	printf("%s: switch count %lld\n", name, coro_switch_count(this));
 	other_function(name, 1);
 	printf("%s: switch count after other function %lld\n", name,
-	       coro_switch_count(this));
+		   coro_switch_count(this));
 
 	my_context_delete(ctx);
 	/* This will be returned from coro_status(). */
 	return 0;
 }
 
-int
-main(int argc, char **argv)
+static char *readFile(char *filename)
 {
+	FILE *fptr = fopen(filename, "r");
+
+	fseek(fptr, 0L, SEEK_END);
+	int size = ftell(fptr) + 1;
+
+	rewind(fptr);
+
+	char *mystring = malloc(sizeof(char) * size);
+	fgets(mystring, size, fptr);
+	fclose(fptr);
+
+	return mystring;
+}
+
+static int parseArraySize(char *str)
+{
+	int size = 0;
+	char *token = strtok(str, " ");
+	while (token != NULL)
+	{
+		token = strtok(NULL, " ");
+		size++;
+	}
+
+	return size;
+}
+
+static int *parseNumbers(char *str, int *arraySize)
+{
+	int i = 0;
+	int *numbers = malloc(sizeof(int) * *arraySize);
+	char *token = strtok(str, " ");
+	while (token != NULL)
+	{
+		numbers[i] = atoi(token);
+		token = strtok(NULL, " ");
+		i++;
+	}
+	return numbers;
+}
+
+int main(int argc, char **argv)
+{
+	char *input = readFile("test.txt");
+
+	char *inputCopy = malloc(sizeof(char) * strlen(input));
+	strcpy(inputCopy, input);
+
+	int arraySize = parseArraySize(inputCopy);
+
+	int *numbers = parseNumbers(input, &arraySize);
+
+	int x = numbers[0];
+	printf("%d\n", x);
+
 	/* Delete these suppressions when start using the args. */
 	(void)argc;
 	(void)argv;
 	/* Initialize our coroutine global cooperative scheduler. */
 	coro_sched_init();
 	/* Start several coroutines. */
-	for (int i = 0; i < 3; ++i) {
+	for (int i = 0; i < 3; ++i)
+	{
 		/*
 		 * The coroutines can take any 'void *' interpretation of which
 		 * depends on what you want. Here as an example I give them
@@ -100,7 +157,8 @@ main(int argc, char **argv)
 	}
 	/* Wait for all the coroutines to end. */
 	struct coro *c;
-	while ((c = coro_sched_wait()) != NULL) {
+	while ((c = coro_sched_wait()) != NULL)
+	{
 		/*
 		 * Each 'wait' returns a finished coroutine with which you can
 		 * do anything you want. Like check its exit status, for
