db315636cadaa0cf19ad20e654f47b810b8d9bf7
IVIosab <mosab.f.r@gmail.com>
2023-09-25 01:58:02 +0300

    Modify makefile and Add time quantum condition

--- 1/solution.c
+++ 1/solution.c
@@ -1,16 +1,103 @@
+// #define _POSIX_C_SOURCE 199309L
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "libcoro.h"
 #include <malloc.h>
 #include "../utils/heap_help/heap_help.h"
+#include <time.h>
 
-/**
- * You can compile and run this code using the commands:
- *
- * $> gcc solution.c libcoro.c
- * $> ./a.out
- */
+static char *readFile(char *filename);						   // read file and return string
+static int *parseNumbers(char *str);						   // parse string to array of numbers
+static void writeFile(char *filename, int *numbers, int size); // write array of numbers to file
+/*MergeSort functions*/
+static int *merge(int *a, int *b);					 // merge two sorted arrays
+static int *mergeSort(int *numbers, void *context);	 // sort array using merge sort
+static int *mergeSortArrays(int **arrays, int size); // sort array of sorted arrays using merge sort
+
+struct my_context
+{
+	char *name;			   // filename
+	int **sorted;		   // pointer to sorted array of the file
+	double quantum;		   // T/N , stored in microseconds
+	struct timespec start; // start time of the coroutine
+};
+
+static struct my_context *
+my_context_new(const char *name, int **sorted, double quantum)
+{
+	struct my_context *ctx = malloc(sizeof(*ctx));
+	ctx->name = strdup(name);
+	ctx->sorted = sorted;
+	ctx->quantum = quantum;
+
+	return ctx;
+}
+
+static void
+my_context_delete(struct my_context *ctx)
+{
+	free(ctx->name);
+	free(ctx);
+}
+
+static int
+coroutine_func_f(void *context)
+{
+	// struct coro *this = coro_this();
+	struct my_context *ctx = context;
+
+	// start timer
+	clock_gettime(CLOCK_MONOTONIC, &ctx->start);
+	int **sorted = ctx->sorted;
+
+	char *input = readFile(ctx->name);
+	int *numbers = parseNumbers(input);
+	sorted[0] = mergeSort(numbers, ctx);
+
+	free(numbers);
+	free(input);
+	my_context_delete(ctx);
+	// printf("coroutine switch count: %lld\n", coro_switch_count(this));
+	return 0;
+}
+
+int main(int argc, char **argv)
+{
+	int T = atof(argv[1]);
+	int *sortedFiles[argc - 1];
+	coro_sched_init();
+
+	for (int i = 2; i < argc; i++)
+	{
+		char *filename = argv[i];
+		coro_new(coroutine_func_f, my_context_new(filename, &sortedFiles[i - 2], T / (argc - 2.0)));
+	}
+
+	struct coro *c;
+	while ((c = coro_sched_wait()) != NULL)
+	{
+		coro_delete(c);
+	}
+
+	int *sortedArr = mergeSortArrays(sortedFiles, argc - 2);
+	int *writeArr = malloc(sizeof(int) * (sortedArr[0]));
+	for (int i = 0; i < sortedArr[0]; i++)
+	{
+		writeArr[i] = sortedArr[i + 1];
+	}
+	writeFile("result.txt", writeArr, sortedArr[0]);
+
+	if (argc > 3)
+		free(sortedArr);
+	free(writeArr);
+	for (int i = 2; i < argc; i++)
+	{
+		free(sortedFiles[i - 2]);
+	}
+
+	return 0;
+}
 
 static char *readFile(char *filename)
 {
@@ -41,9 +128,8 @@ static int *parseNumbers(char *str)
 		size++;
 	}
 
-	free(strCopy); // free char *strCopy
+	free(strCopy);
 
-	// parse numbers
 	int i = 1;
 	int *numbers = malloc(sizeof(int) * (size + 1));
 	numbers[0] = size;
@@ -105,8 +191,9 @@ static int *merge(int *a, int *b)
 	return merged;
 }
 
-static int *mergeSort(int *numbers)
+static int *mergeSort(int *numbers, void *context)
 {
+	struct my_context *ctx = context;
 	int size = numbers[0];
 	if (size == 1)
 	{
@@ -127,10 +214,20 @@ static int *mergeSort(int *numbers)
 		{
 			right[i - mid] = numbers[i];
 		}
-		int *sortedLeft = mergeSort(left);
-		int *sortedRight = mergeSort(right);
+		int *sortedLeft = mergeSort(left, ctx);
+		int *sortedRight = mergeSort(right, ctx);
 		int *sorted = merge(sortedLeft, sortedRight);
-		coro_yield();
+
+		struct timespec end;
+		clock_gettime(CLOCK_MONOTONIC, &end);
+		double x = 1000000.0 * end.tv_sec + 1e-3 * end.tv_nsec - (1000000.0 * ctx->start.tv_sec + 1e-3 * ctx->start.tv_nsec);
+		if (x > ctx->quantum) // check if time elapsed is greater than quantum
+		{
+			// yield and restart timer
+			coro_yield();
+			clock_gettime(CLOCK_MONOTONIC, &ctx->start);
+		}
+
 		free(left);
 		free(right);
 		if (mid > 1)
@@ -140,56 +237,6 @@ static int *mergeSort(int *numbers)
 		return sorted;
 	}
 	return NULL;
-}
-
-struct my_context
-{
-	int id;
-	char *name;
-	int **sorted;
-};
-
-static struct my_context *
-my_context_new(const char *name, int id, int **sorted)
-{
-	struct my_context *ctx = malloc(sizeof(*ctx));
-	ctx->name = strdup(name);
-	ctx->sorted = sorted;
-	ctx->id = id;
-
-	return ctx;
-}
-
-static void
-my_context_delete(struct my_context *ctx)
-{
-	free(ctx->name);
-	free(ctx);
-}
-
-/**
- * Coroutine body. This code is executed by all the coroutines. Here you
- * implement your solution, sort each individual file.
- */
-static int
-coroutine_func_f(void *context)
-{
-	// struct coro *this = coro_this();
-	struct my_context *ctx = context;
-	int **sorted = ctx->sorted;
-
-	char *input = readFile(ctx->name);
-
-	int *numbers = parseNumbers(input);
-
-	sorted[0] = mergeSort(numbers);
-
-	free(numbers);
-
-	free(input);
-
-	my_context_delete(ctx);
-	return 0;
 }
 
 static int *mergeSortArrays(int **arrays, int size)
@@ -223,42 +270,4 @@ static int *mergeSortArrays(int **arrays, int size)
 		return sorted;
 	}
 	return NULL;
-}
-
-int main(int argc, char **argv)
-{
-	int *sortedFiles[argc];
-	coro_sched_init();
-
-	for (int i = 1; i < argc; i++)
-	{
-		char *filename = argv[i];
-		coro_new(coroutine_func_f, my_context_new(filename, i - 1, &sortedFiles[i - 1]));
-	}
-
-	/* Wait for all the coroutines to end. */
-	struct coro *c;
-	while ((c = coro_sched_wait()) != NULL)
-	{
-		printf("Finished %d\n", coro_status(c));
-		coro_delete(c);
-	}
-
-	int *sortedArr = mergeSortArrays(sortedFiles, argc - 1);
-	int *writeArr = malloc(sizeof(int) * (sortedArr[0]));
-	for (int i = 0; i < sortedArr[0]; i++)
-	{
-		writeArr[i] = sortedArr[i + 1];
-	}
-	writeFile("result.txt", writeArr, sortedArr[0]);
-
-	if (argc > 2)
-		free(sortedArr);
-	free(writeArr);
-	for (int i = 1; i < argc; i++)
-	{
-		free(sortedFiles[i - 1]);
-	}
-
-	return 0;
 }
