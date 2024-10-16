6bec4933d5e53e9b21294f9fc42ac1f05360dd1e
Ilya Petrovanov <ispetrovanov@edu.hse.ru>
2024-10-17 01:37:12 +0300

    add support hw2

--- DockAllcups.dockerfile
+++ DockAllcups.dockerfile
@@ -1,7 +1,9 @@
 FROM ubuntu:noble
-RUN apt update
-RUN DEBIAN_FRONTEND=noninteractive TZ=Etc/UTC apt -y install tzdata
-RUN apt install -y clang make
+RUN export DEBIAN_FRONTEND=noninteractive && \
+    TZ=Etc/UTC && \
+    apt-get update && \
+    apt-get install -y --no-install-recommends \
+    tzdata unzip jq python3.7 clang make
 RUN ln /usr/bin/clang /usr/bin/gcc
 RUN mkdir /sysprog
 
@@ -13,13 +15,4 @@ ENV RESOURCES_DIR_MOUNT='/tmp/data'
 ENV SOLUTION_MOUNT='/sysprog/solution'
 ENV RESULT_LOCATION='/sysprog/results/output.json'
 
-RUN mkdir /sysprog/3
-ADD 3/test.c /sysprog/3/
-
-RUN mkdir /sysprog/4
-ADD 4/test.c /sysprog/4/
-
-ADD utils /sysprog/utils
-ADD test.sh /sysprog/test.sh
-
-# CMD ["python3", "main.py"]
+CMD ["/tmp/data/test.sh"]
--- test.sh
+++ test.sh
@@ -2,11 +2,50 @@
 
 # Build all *.c files. And then + leaks.
 
-cp src/test.c ./solution/
-cd solution
-make
-if ./test; then
-	echo "Passed!"
-else
-	echo "Fail!"
+hw_number=$(jq -r '.hw_number' "$RESOURCES_DIR_MOUNT/hw_number.json") 
+
+unzip -o $SOLUTION_MOUNT/solution.zip -d $SOLUTION_MOUNT/
+
+if [ "$hw_number" -eq 1 ]; then
+	# In Work ...
+	echo ""
+elif [ "$hw_number" -eq 2 ]; then
+    cp $RESOURCES_DIR_MOUNT/checker.py $SOLUTION_MOUNT
+	cp $RESOURCES_DIR_MOUNT/Makefile $SOLUTION_MOUNT
+	cd $SOLUTION_MOUNT
+	make
+	if python3 $SOLUTION_MOUNT/checker.py --max 25; then
+		status="OK"
+		score="25"
+	elif python3 $SOLUTION_MOUNT/checker.py --max 20; then
+		status="OK"
+		score="20"
+	elif python3 $SOLUTION_MOUNT/checker.py --max 15; then
+		status="OK"
+		score="15"
+	else 
+		status="FAIL"
+		score="0"
+	fi
+elif [ "$hw_number" -eq 3 ] || [ "$hw_number" -eq 4 ] || [ "$hw_number" -eq 5 ]; then
+	# In work ...
+	cp $RESOURCES_DIR_MOUNT/test.c $SOLUTION_MOUNT
+	cp $RESOURCES_DIR_MOUNT/Makefile $SOLUTION_MOUNT
+	cd $SOLUTION_MOUNT
+	make
+	if ./test; then
+		status="OK"
+		score="15"
+	else
+		status="FAIL"
+		score=0
+	fi
 fi
+
+	cat << EOF > $RESULT_LOCATION
+{
+	"status": $status,
+	"validationQuality": $score,
+	"testingQuality": $score
+}
+EOF
