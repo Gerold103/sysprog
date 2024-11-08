56cd828a1a78e103e5ac9c72ec99f345d08c4f5d
Ilya Petrovanov <ispetrovanov@edu.hse.ru>
2024-11-09 00:24:18 +0300

    test.sh: apply diff from PR review

--- test.sh
+++ test.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
 
-# Build all *.c files. And then + leaks.
-
-hw=$(jq -r '.hw' "$RESOURCES_DIR_MOUNT/settings.json") 
-
-cp -r $RESOURCES_DIR_MOUNT/* /sysprog
-
-if [ "$NOZIP" == "1" ]; then 
+if [ "$IS_LOCAL" == "1" ]; then
+	hw=$HW
+	SOLUTION_MOUNT="$RESOURCES_DIR_MOUNT/$hw"
 	cp $SOLUTION_MOUNT/* /sysprog/solution
 else
 	unzip -o $SOLUTION_MOUNT -d /sysprog/solution
+	hw=$(jq -r '.hw' "$RESOURCES_DIR_MOUNT/settings.json")
 fi
 
-if [ "$hw" -eq 1 ]; then
-	# In Work ...
-	echo ""
-elif [ "$hw" -eq 2 ]; then
+cp -r $RESOURCES_DIR_MOUNT/* /sysprog
+
+status='"ERR"'
+score=0
+
+if [ "$hw" -eq 2 ]; then
     cp $RESOURCES_DIR_MOUNT/2/checker.py /sysprog/solution
 	cp $RESOURCES_DIR_MOUNT/2/Makefile /sysprog/solution
 	rm /sysprog/solution/parser_test.c
 	cd /sysprog/solution
 
+	echo '🔨 Building'
 	make test_glob
-	
+
+	echo '⏳ Running tests'
 	if python3 checker.py --with_logic 1 --with_background 1 -e ./mybash; then
 		status='"OK"'
 		score="25"
@@ -35,35 +36,37 @@ elif [ "$hw" -eq 2 ]; then
 	elif python3 checker.py -e ./mybash; then
 		status='"OK'
 		score="15"
-	else 
-		status='"ERR"'
-		score="0"
 	fi
 elif [ "$hw" -eq 3 ] || [ "$hw" -eq 4 ] || [ "$hw" -eq 5 ]; then
 	cp $RESOURCES_DIR_MOUNT/"$hw"/test.c /sysprog/solution
 	cp $RESOURCES_DIR_MOUNT/"$hw"/Makefile /sysprog/solution
-
-	if [ "$hw" -eq 5 ]; then
-		rm /sysprog/solution/*_exe.c
-	fi
-
+	rm -f /sysprog/solution/*_exe.c
 	cd /sysprog/solution
-	
+
+	echo '🔨 Building'
 	make test_glob
 
+	echo '⏳ Running tests'
 	if ./test; then
 		status='"OK"'
 		score=$(./test --max_points)
-	else
-		status='"ERR"'
-		score=0
 	fi
+else
+	echo "Unknown or unsupported HW number"
 fi
 
-	cat << EOF > $RESULT_LOCATION
+echo '⚖️ Result:'
+if [ "$IS_LOCAL" == "1" ]; then
+	echo "Status: $status, points: $score"
+else
+	result=$(cat << EOF
 {
 	"status": $status,
 	"validationQuality": $score,
 	"testingQuality": $score
 }
 EOF
+	)
+	echo "$result"
+	echo $result > $RESULT_LOCATION
+fi
